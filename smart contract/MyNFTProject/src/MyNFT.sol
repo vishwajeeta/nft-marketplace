ba29a022a998acf664b3830cdf2249007cef1fa5
vishwajeeta <gs699542@gmail.com>
2025-08-14 22:44:09 +0530

    created and deployed smart contract

---
+++ smart contract/MyNFTProject/src/MyNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+
+    function mintNFT(address to, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
